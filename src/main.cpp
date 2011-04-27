9e44faf0d5f20e68e7e4fe29dce6076bc85b051f
Jiri Pinkava <jiri.pinkava@vscht.cz>
2011-04-28 01:38:45 +0200

    Set app. name, org. name and domain

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName("TransRec");
+    a.setOrganizationName("Pinky");
+    a.setOrganizationDomain("web.vscht.cz/pinkavaj");
     MainWindow w;
     w.show();
 
